9946f61b5829942ae66cefd940062a7c48dcb457
qmya <qandeelgul90@gmail.com>
2021-01-14 17:05:54 -0500

    From callback Hell to Promises

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
+  //   superagent
+  //     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+  //     .end((err, res) => {
+  //       if (err) return console.log(err.message);
+  //       console.log(res.body.message);
+  //       //this will create a new file and add the image of the dog to it
+  //       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+  //         if (err) return console.log(err.message);
+  //         console.log('Random dog image saved to the file');
+  //       });
+  //     });
+  // });
+
+  //using the Promises 👇🏽 to change the ☝🏽 code from callback hell
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
+    .then((result) => {
+      console.log(result.body.message);
       //this will create a new file and add the image of the dog to it
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+      fs.writeFile('dog-img.txt', result.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log('Random dog image saved to the file');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
