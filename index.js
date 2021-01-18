6a18e2118d98a487c0369380a4c546c032a951cb
qmya <qandeelgul90@gmail.com>
2021-01-18 08:48:21 -0500

    Building Promises

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
 const fs = require('fs');
 const superagent = require('superagent');
-//__dirname : ./
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  //   superagent
-  //     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-  //     .end((err, res) => {
-  //       if (err) return console.log(err.message);
-  //       console.log(res.body.message);
-  //       //this will create a new file and add the image of the dog to it
-  //       fs.writeFile('dog-img.txt', res.body.message, (err) => {
-  //         if (err) return console.log(err.message);
-  //         console.log('Random dog image saved to the file');
-  //       });
-  //     });
-  // });
-
-  //using the Promises 👇🏽 to change the ☝🏽 code from callback hell
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((result) => {
-      console.log(result.body.message);
-      //this will create a new file and add the image of the dog to it
-      fs.writeFile('dog-img.txt', result.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to the file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+//Pro is Promise
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    //herewe do readfilework / Async work
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😢');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write the file 😢');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((result) => {
+    console.log(result.body.message);
+    //this will create a new file and add the image of the dog to it
+
+    return writeFilePro('dog-img.txt', result.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to the file');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//__dirname : ./
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
+//       //this will create a new file and add the image of the dog to it
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//         console.log('Random dog image saved to the file');
+//       });
+//     });
+// });
+
+//using the Promises 👇🏽 to change the ☝🏽 code from callback hell
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((result) => {
+//       console.log(result.body.message);
+//       //this will create a new file and add the image of the dog to it
+//       fs.writeFile('dog-img.txt', result.body.message, (err) => {
+//         if (err) return console.log(err.message);
+//         console.log('Random dog image saved to the file');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
